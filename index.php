7c8784e79f8c0ce6207cffa77b3872ed812e6a08
John Cullen <john.cullen@salsadigital.com.au>
2016-07-24 06:21:47 +1000

    added temp favicon.ico image

--- index.php
+++ index.php
@@ -10,20 +10,15 @@ $feedback = $obj->getSetup();
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Tracker</title>
-        <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://bootswatch.com/yeti/bootstrap.css" />
-        <!-- Font Awesome 4.4.0 -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
+        <link rel="shortcut icon" href="./TimeTracking/Assets/images/favicon.ico" />
         <link rel="stylesheet" href="./TimeTracking/Assets/css/styles.css" />
-        <style>
-
-        </style>
     </head>
     <body>
+        <?php print $obj->index();?> 
         <div id='feedback'>
             <?php echo $feedback; ?>
         </div>
-        <?php print $obj->index();?>
-        
     </body>
 </html>
